719a629023f3fd069485f29ce37f53d068fe171c
EvgenyShcherbakov <e.shcherbakov@gmail.com>
2024-10-25 13:57:24 +0200

    Class skeleton

---
+++ src/PicnicScanner1D.java
@@ -0,0 +1,24 @@
+public class PicnicScanner1D {
+
+    public void slotsSchedule() {
+        // The actual solution to the challenge
+    }
+
+    public void makeItemsList() {
+        // Generate a list of items that can fit in
+    }
+
+    public void modifyItemsList() {
+        // Modify the list to make items unfit
+    }
+
+    public void testSchedule() {
+        // Validate the outcome of slotsSchedule()
+    }
+
+    public static void main(String[] args) {
+        PicnicScanner1D scanner = new PicnicScanner1D();
+        // Call methods here for testing
+    }
+}
+
