bb5c9a81593731e667ee06e7d7ebc388d1882fcb
EvgenyShcherbakov <e.shcherbakov@gmail.com>
2024-10-26 13:52:31 +0200

    Initial sorting implementation

--- src/PicnicScanner1D.java
+++ src/PicnicScanner1D.java
@@ -1,7 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class ItemWithIndex {
+    int item;
+    int index;
+
+    ItemWithIndex(int item, int index) {
+        this.item = item;
+        this.index = index;
+    }
+}
+
 public class PicnicScanner1D {
 
-    public void slotsSchedule() {
-        // The actual solution to the challenge
+    public List<Integer> slotsSchedule(int nSlots, int slotSize, List<Integer> items) {
+        List<Integer> ans = new ArrayList<>(Collections.nCopies(items.size(), 0));
+
+        // Make a list of items with their indices
+        List<ItemWithIndex> itemsWithIndices = new ArrayList<>();
+        for (int i = 0; i < items.size(); i++) {
+            itemsWithIndices.add(new ItemWithIndex(items.get(i), i));
+        }
+
+        // Sort items in descending order using List.sort
+        itemsWithIndices.sort((a, b) -> {
+            int itemComparison = Integer.compare(b.item, a.item);
+            if (itemComparison != 0) {
+                return itemComparison;
+            } else {
+                return Integer.compare(a.index, b.index); // Use index to break ties
+            }
+        });
+
+        // Temporary print for testing
+        System.out.println("Sorted items with indices:");
+        for (ItemWithIndex itemWithIndex : itemsWithIndices) {
+            System.out.println("Item: " + itemWithIndex.item + ", Index: " + itemWithIndex.index);
+        }
+
+        return ans;
     }
 
     public void makeItemsList() {
@@ -18,7 +56,8 @@ public class PicnicScanner1D {
 
     public static void main(String[] args) {
         PicnicScanner1D scanner = new PicnicScanner1D();
-        // Call methods here for testing
+        List<Integer> items = List.of(5, 3, 8, 1, 7);
+        scanner.slotsSchedule(10, 5, items);
     }
 }
 
